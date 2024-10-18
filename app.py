1a04f5388c543732b4da199a35ddfe750673b5fb
ARLIKIN <nik5552000@mail.ru>
2024-10-19 01:40:26 +0400

    добавил авторизацию, отдельно обработал отсутствия имени ключа, переименовал исполняемые файлы

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+from functools import wraps
 
 from http import HTTPStatus
 from dotenv import load_dotenv
@@ -11,6 +12,8 @@ app = Flask(__name__)
 
 DIR_KEY_TEMPLATE = 'wg/static/wg0-client-{name}.conf'
 STATIC_DIR = 'wg/static'
+ADMIN_USERNAME = os.getenv('ADMIN_USERNAME')
+ADMIN_PASSWORD = os.getenv('ADMIN_PASSWORD')
 
 
 def execute_command(command):
@@ -30,24 +33,45 @@ def read_config(key):
     return None
 
 
+def auth_required(f):
+    @wraps(f)
+    def decorator(*args, **kwargs):
+        auth = request.authorization
+        if (
+            not auth or auth.username != ADMIN_USERNAME
+            or auth.password != ADMIN_PASSWORD
+        ):
+            return make_response(
+                jsonify({"success": False, "error": "Not Authorized"}),
+                HTTPStatus.UNAUTHORIZED
+            )
+        return f(*args, **kwargs)
+    return decorator
+
+
+def error_check_exist_user_id():
+    return make_response(
+        jsonify({"success": False, "error": "Not user id"}),
+        HTTPStatus.BAD_REQUEST
+    )
+
+
 @app.route("/len", methods=['GET'])
+@auth_required
 def get_user_count():
     user_count = sum(len(files) for _, _, files in os.walk(STATIC_DIR))
-    return jsonify({"success": False, "user_count": user_count})
+    return jsonify({"success": True, "user_count": user_count})
 
 
 @app.route("/createWG", methods=['POST'])
+@auth_required
 def create_user():
     js = request.get_json()
-    key = js.get('chat_id')
+    key = js.get('name_key')
     if not key:
-        return make_response(
-            jsonify({"success": False, "error": "Not Authorized"}),
-            HTTPStatus.UNAUTHORIZED
-        )
-
+       return error_check_exist_user_id()
     if not os.path.isfile(DIR_KEY_TEMPLATE.format(name=key)):
-        if not execute_command(f'wg/addusertovpn.sh {key}'):
+        if not execute_command(f'wg/add_user.sh {key}'):
             return make_response(
                 jsonify(
                     {"success": False, "error": "Failed to create user"}
@@ -71,15 +95,13 @@ def create_user():
 
 
 @app.route("/deleteWG", methods=['DELETE'])
+@auth_required
 def delete_user():
     js = request.get_json()
-    key = js.get('chat_id')
+    key = js.get('name_key')
     if not key:
-        return make_response(
-            jsonify({"success": False, "error": "Not Authorized"}),
-            HTTPStatus.UNAUTHORIZED
-        )
-    if not execute_command(f'wg/deleteuserfromvpn.sh {key}'):
+        return error_check_exist_user_id()
+    if not execute_command(f'wg/delete_user.sh {key}'):
         return make_response(
             jsonify(
                 {"success": False, "error": "Failed to delete user"}
@@ -90,14 +112,12 @@ def delete_user():
 
 
 @app.route("/getConfig", methods=['GET'])
+@auth_required
 def get_config():
     js = request.get_json()
-    key = js.get('chat_id')
+    key = js.get('name_key')
     if not key:
-        return make_response(
-            jsonify({"success": False, "error": "Not Authorized"}),
-            HTTPStatus.UNAUTHORIZED
-        )
+       return error_check_exist_user_id()
     config_content = read_config(key)
     if config_content:
         return make_response(
