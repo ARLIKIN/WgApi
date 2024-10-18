b879dbb366748c4b000813b6e2b076f362475f85
ARLIKIN <nik5552000@mail.ru>
2024-10-19 01:00:02 +0400

    Написал эндпоинты, изменил пути в скриптах, вынес определение хоста и порта в env

--- app.py
+++ app.py
@@ -1,19 +1,39 @@
 import os
-import json
 import subprocess
 
-from flask import Flask, request, make_response, jsonify
+from http import HTTPStatus
+from dotenv import load_dotenv
+from flask import Flask, request, jsonify, make_response
+
+load_dotenv()
 
 app = Flask(__name__)
 
-DIR_KEY = 'wg/static/wg0-client-{name}.conf'
+DIR_KEY_TEMPLATE = 'wg/static/wg0-client-{name}.conf'
+STATIC_DIR = 'wg/static'
+
+
+def execute_command(command):
+    try:
+        subprocess.run(command, shell=True, check=True)
+    except subprocess.CalledProcessError as e:
+        app.logger.error(f"Error executing command: {command}, {e}")
+        return False
+    return True
+
+
+def read_config(key):
+    config_path = DIR_KEY_TEMPLATE.format(name=key)
+    if os.path.isfile(config_path):
+        with open(config_path, 'r') as file:
+            return file.read()
+    return None
 
 
 @app.route("/len", methods=['GET'])
 def get_user_count():
-    dir = 'wg/static'
-    user_count = sum([len(files) for r, d, files in os.walk(dir)])
-    return {"user_count": user_count}
+    user_count = sum(len(files) for _, _, files in os.walk(STATIC_DIR))
+    return jsonify({"success": False, "user_count": user_count})
 
 
 @app.route("/createWG", methods=['POST'])
@@ -22,57 +42,76 @@ def create_user():
     key = js.get('chat_id')
     if not key:
         return make_response(
-            jsonify({"success": False, "error": "Not Authorized"}), 401)
+            jsonify({"success": False, "error": "Not Authorized"}),
+            HTTPStatus.UNAUTHORIZED
+        )
 
-    if not os.path.isfile(DIR_KEY.format(name=str(key))):
-        subprocess.call(f'wg/addusertovpn.sh {str(key)}', shell=True)
+    if not os.path.isfile(DIR_KEY_TEMPLATE.format(name=key)):
+        if not execute_command(f'wg/addusertovpn.sh {key}'):
+            return make_response(
+                jsonify(
+                    {"success": False, "error": "Failed to create user"}
+                ),
+                HTTPStatus.INTERNAL_SERVER_ERROR
+            )
 
-    if os.path.isfile(DIR_KEY.format(name=str(key))):
-        with open(DIR_KEY.format(name=str(key)), "r") as file:
-            config_content = file.read()
-            result = {'config': config_content}
-        return make_response(result, 200,
-                             {'Content-Type': 'json'})
+    config_content = read_config(key)
+    if config_content:
+        return make_response(
+            jsonify({"success": True, "config": config_content}),
+            HTTPStatus.OK
+        )
     else:
         return make_response(
             jsonify(
                 {"success": False, "error": "Configuration file not found"}
             ),
-            404
+            HTTPStatus.NOT_FOUND
         )
 
 
 @app.route("/deleteWG", methods=['DELETE'])
 def delete_user():
-    js = json.loads(request.data)
+    js = request.get_json()
     key = js.get('chat_id')
     if not key:
-        return make_response({"success": False, "error": "Not Authorized"},
-                             401)
-
-    subprocess.call(f'wg/deleteuserfromvpn.sh {str(key)}', shell=True)
-    return {"success": True}
+        return make_response(
+            jsonify({"success": False, "error": "Not Authorized"}),
+            HTTPStatus.UNAUTHORIZED
+        )
+    if not execute_command(f'wg/deleteuserfromvpn.sh {key}'):
+        return make_response(
+            jsonify(
+                {"success": False, "error": "Failed to delete user"}
+            ),
+            HTTPStatus.INTERNAL_SERVER_ERROR
+        )
+    return jsonify({"success": True})
 
 
 @app.route("/getConfig", methods=['GET'])
 def get_config():
-    js = json.loads(request.data)
+    js = request.get_json()
     key = js.get('chat_id')
     if not key:
-        return make_response({"success": False, "error": "Not Authorized"},
-                             401)
-    if os.path.isfile(DIR_KEY.format(name=str(key))):
-        with open(DIR_KEY.format(name=str(key)), 'r') as file:
-            config_data = file.read()
         return make_response(
-            {"config": config_data},
-            200,
-            {'Content-Type': 'json'}
+            jsonify({"success": False, "error": "Not Authorized"}),
+            HTTPStatus.UNAUTHORIZED
+        )
+    config_content = read_config(key)
+    if config_content:
+        return make_response(
+            jsonify({"config": config_content}),
+            HTTPStatus.OK
         )
     else:
         return make_response(
-            {"success": False, "error": "Config not found"}, 404
+            jsonify({"success": False, "error": "Config not found"}),
+            HTTPStatus.NOT_FOUND
         )
 
+
 if __name__ == '__main__':
-    app.run("0.0.0.0", port=8080)
+    host = os.getenv('FLASK_HOST', '0.0.0.0')
+    port = int(os.getenv('FLASK_PORT', 8080))
+    app.run(host=host, port=port)
